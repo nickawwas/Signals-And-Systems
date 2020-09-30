ff60ecf7a0075c3e65d5bbcc0919254590c83460
Nicholas Kawwas <68450354+nickawwas@users.noreply.github.com>
2020-09-29 23:29:53 -0400

    Create question3a.m

---
+++ Lab-1/question3a.m
@@ -0,0 +1,12 @@
+% Nicholas Kawwas (40124338)
+% ELEC 342 UK-X Lab 1 - Question 3 a)
+ 
+clc;
+clear all;
+close all;
+ 
+x = [0:4*1024];
+y1 = cos( (2*pi)/1024 * x );
+y2 = cos( (2*pi)/1024 * (x + 1024));
+ 
+ans = sum(y2) - sum(y1)
